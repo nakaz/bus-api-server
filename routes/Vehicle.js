a1b7c8d3ab144be35bd548cedc64ead5d469ff3f
Sean Nakamura <seanenakamura@gmail.com>
2017-04-07 18:24:11 -1000

    Vehicle route to use helper http request

--- routes/Vehicle.js
+++ routes/Vehicle.js
@@ -3,7 +3,7 @@ const {
   express,
   router,
   BUS_KEY,
-  BusDataParser
+  BusAPIQuery
 } = require('./helpers')
 
 const VEHICLE_URL = 'http://api.thebus.org/vehicle/?key=';
@@ -13,23 +13,14 @@ const VEHICLE_URI = `${VEHICLE_URL}${BUS_KEY}${VEHICLE_PARAM}`;
 
 router.get('/:vehicleId', (req, res) => {
   const vehicleId = req.params.vehicleId
-  http.get(`${VEHICLE_URI}${vehicleId}`, function (resp){
-    let data = '';
-    resp.on('data', function (chunk){
-      data += chunk;
-    });
-    resp.on('end', function (){
-      BusDataParser(data)
-        .then(result => res.json(result))
-        .catch(err => {
-          res.status(500)
-          res.json({error: err})
-        })
-    });
-  }).on('error', function (err){
-    res.status(500)
-    res.json({error: err})
-  });
+  BusAPIQuery(VEHICLE_URI, vehicleId)
+    .then(result => {
+      res.json(result)
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({error: err})
+    })
 })
 
 module.exports = router
