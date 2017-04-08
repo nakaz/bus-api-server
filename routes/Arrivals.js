764f598db500de88318068b78f6fa6237c25143a
Sean Nakamura <seanenakamura@gmail.com>
2017-04-07 18:21:25 -1000

    Arrivals to use helper http request

--- routes/Arrivals.js
+++ routes/Arrivals.js
@@ -3,7 +3,7 @@ const {
   express,
   router,
   BUS_KEY,
-  BusDataParser
+  BusAPIQuery
 } = require('./helpers')
 
 const ARRIVAL_URL = 'http://api.thebus.org/arrivals/?key=';
@@ -13,23 +13,14 @@ const ARRIVAL_URI = `${ARRIVAL_URL}${BUS_KEY}${STOP_PARAM}`
 
 router.get('/stop/:stopId', (req, res) => {
   const stopId = req.params.stopId
-  http.get(`${ARRIVAL_URI}${stopId}`, function (resp){
-    let data = '';
-    resp.on('data', function (chunk){
-      data += chunk;
-    });
-    resp.on('end', function (){
-      BusDataParser(data)
-        .then(result => res.json(result))
-        .catch(err => {
-          res.status(500)
-          res.json({error: err})
-        })
-    });
-  }).on('error', function (err){
-    res.status(500)
-    res.json({error: err})
-  });
+  BusAPIQuery(ARRIVAL_URI, stopId)
+    .then(result => {
+      res.json(result)
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({error: err})
+    })
 })
 
 module.exports = router
