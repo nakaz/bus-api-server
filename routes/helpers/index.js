6cb08f00cb6cb53135bd2b497056e5c8d66928b3
Sean Nakamura <seanenakamura@gmail.com>
2017-04-07 18:19:43 -1000

    helpers provide http queries

--- routes/helpers/index.js
+++ routes/helpers/index.js
@@ -4,11 +4,32 @@ const router = express.Router()
 const BUS_KEY = process.env.DABUS_API_KEY;
 const BusDataParser = require('../../lib/BusDataParser')
 
+const BusAPIQuery = (URI, value) => {
+  return new Promise ((resolve, reject) => {
+    http.get(`${URI}${value}`, function (resp){
+      let data = '';
+      resp.on('data', function (chunk){
+        data += chunk;
+      });
+      resp.on('end', function (){
+        BusDataParser(data)
+          .then(result => resolve(result))
+          .catch(err => {
+            reject(err)
+          })
+      });
+    }).on('error', function (err){
+      reject(err)
+    });
+  })
+}
+
 module.exports = {
   http,
   express,
   router,
   BUS_KEY,
+  BusAPIQuery,
   BusDataParser
 }
 
